4e7df76fc4501448c266afb0b4274962c9f265ca
Aurorn <aurornispro@gmail.com>
2024-06-10 13:48:02 -0400

    fixed errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 module.exports = {
   entry: './src/index.js',
@@ -31,10 +27,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new webpack.DefinePlugin({
-      'process.env.REACT_APP_API_KEY': JSON.stringify(process.env.REACT_APP_API_KEY),
+      template: './src/index.html',
     }),
   ],
   resolve: {
