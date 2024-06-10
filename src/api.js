2bd065d5623ac54d7acf7dd7d488b7621a737d7a
Aurorn <aurornispro@gmail.com>
2024-06-10 12:07:56 -0400

    Added API source + protection

---
+++ src/api.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_URL = 'https://api.weatherapi.com/v1';
+
+export const fetchCurrentWeather = async (location) => {
+  const response = await fetch(`${BASE_URL}/current.json?key=${API_KEY}&q=${location}`);
+  const data = await response.json();
+  return data;
+};
+
+export const fetchForecast = async (location) => {
+  const response = await fetch(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${location}&days=14`);
+  const data = await response.json();
+  return data;
+};
